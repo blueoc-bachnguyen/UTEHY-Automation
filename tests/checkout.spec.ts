23cc2a17e958706642a92ad0d55ed983dfd5894f
blueoc-vanlinh <linh.nguyenvan@blueoc.tech>
2025-11-24 11:12:31 +0700

    test CI/CD

---
+++ tests/checkout.spec.ts
@@ -0,0 +1,36 @@
+import { test, expect } from '@playwright/test';
+import { LoginPage } from '../page/login.page';
+import { InventoryPage } from '../page/InventoryPage';
+import { CartPage } from '../page/cart.page';
+import { CheckoutPage } from '../page/checkout.page';
+
+
+test('complete checkout', async ({ page }) => {
+const login = new LoginPage(page);
+const inventory = new InventoryPage(page);
+const cart = new CartPage(page);
+const checkout = new CheckoutPage(page);
+
+
+await login.goto('/');
+await login.login('standard_user', 'secret_sauce');
+
+
+await inventory.addProduct('Sauce Labs Backpack');
+await inventory.addProduct('Sauce Labs Bike Light');
+
+
+await page.click('.shopping_cart_link');
+await cart.checkout();
+
+
+await checkout.fillCustomer('John', 'Doe', '12345');
+await checkout.continue();
+await checkout.finish();
+
+
+await expect(page.locator('.complete-header')).toHaveText(/thank you for your order/i);
+
+
+await inventory.logout();
+});
